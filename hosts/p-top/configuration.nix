0d1a79d051623398163d531b63f249577665edb4
p <p.harris050406@gmail.com>
2026-07-09 11:44:46 -0700

    more setup for laptop

---
+++ hosts/p-top/configuration.nix
@@ -0,0 +1,12 @@
+{config, lib, pkgs, ...}:{
+	imports = [
+		./hardware-configuration.nix
+	];
+
+	networking.hostName = "p-desk";
+	system.stateVersion="26.05";
+	
+	environment.systemPackages = with pkgs;[
+		# desktop-specific system packages go here	
+	];
+}
